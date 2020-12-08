20956ec7c1663a66305f77b9da93b7758997095b
agrippon <alice.grippon@culture.gouv.fr>
2020-12-08 20:33:03 +0100

    r15_doc_tech

---
+++ ressources/DocCourante/html/installation/conf.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# General information about the project.
+project = u'VITAM - Documentation d\'installation'
+doc_id = 'vitam-documentation-installation'
+
+execfile("../conf-common.py")
