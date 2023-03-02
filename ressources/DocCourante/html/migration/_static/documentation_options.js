d62777f01b2b4b3b89cc051e9663a632b1c9fcbd
ALICE GRIPPON <56266198+agrippon@users.noreply.github.com>
2023-03-02 15:22:10 +0100

    update doc tech

--- ressources/DocCourante/html/migration/_static/documentation_options.js
+++ ressources/DocCourante/html/migration/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.0',
+    VERSION: '6.rc.1',
     LANGUAGE: 'fr',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
